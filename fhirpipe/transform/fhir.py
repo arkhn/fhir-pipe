ac75cada9847f67033b9623f1fbe60ac9c4bfaf6
Jasopaum <jason.paumier@gmail.com>
2020-01-06 16:16:54 +0100

    put back reference handling + test

--- fhirpipe/transform/fhir.py
+++ fhirpipe/transform/fhir.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 
 from fhirpipe.utils import build_col_name, new_col_name
+from fhirpipe.load.fhirstore import find_fhir_resource
 
 
 def create_resource(chunk, resource_structure):
@@ -60,6 +61,7 @@ def rec_create_fhir_object(fhir_obj, attribute_structure, row):
 
             fhir_obj[attribute_structure["name"]] = result
 
+        # Otherwise, we can recurse on children
         else:
             if attribute_structure["type"] == "list":
                 fhir_obj[attribute_structure["name"]] = []
@@ -85,10 +87,11 @@ def rec_create_fhir_object(fhir_obj, attribute_structure, row):
                         fhir_obj[attribute_structure["name"]], attr, row
                     )
 
-            # TODO reference
-            # If the object is a Reference, to we give it to bind_reference
-            # if attribute_structure["type"].startswith("Reference"):
-            #     bind_reference(fhir_obj[attribute_structure["name"]], attribute_structure)
+                # If the object is a Reference, to we give it to bind_reference
+                if attribute_structure["type"].startswith("Reference"):
+                    bind_reference(
+                        fhir_obj[attribute_structure["name"]], attribute_structure
+                    )
 
     # If the current object is a list, we can repeat the same steps as above for each item
     elif isinstance(fhir_obj, list) and len(fhir_obj) > 0:
@@ -97,3 +100,42 @@ def rec_create_fhir_object(fhir_obj, attribute_structure, row):
             children.append(rec_create_fhir_object(dict(), child_spec, row))
 
         fhir_obj[attribute_structure["name"]] = children
+
+
+def bind_reference(fhir_object, fhir_spec):
+    """
+    Analyse a reference and replace the provided identifier
+    with official FHIR uri if the resource reference exists.
+
+    args:
+        fhir_object: the fhir object to parse
+
+    return:
+        a fhir_object where valid references are now FHIR uris
+    """
+
+    # First we check that the reference has been provided
+    if fhir_object and fhir_object["identifier"]["value"]:
+        identifier = fhir_object["identifier"]["value"]
+
+        # Collect all the resource_types which could be referenced
+        try:
+            s = fhir_spec["type"]
+            resource_types = s[s.find("(") + 1 : s.find(")")].split("|")
+        except AttributeError:
+            raise ReferenceError(
+                f"No FHIR Resource type provided for the reference {fhir_spec['name']}"
+            )
+
+        # Search for a fhir instance among the listed
+        # resources and exit when one is found
+        fhir_uri = None
+        for resource_type in resource_types:
+            fhir_uri = find_fhir_resource(resource_type, identifier)
+            if fhir_uri is not None:
+                break
+
+        # If an instance was found, replace the provided
+        # identifier with FHIR id found
+        if fhir_uri is not None:
+            fhir_object["identifier"]["value"] = fhir_uri
