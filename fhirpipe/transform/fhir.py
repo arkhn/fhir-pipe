281d7520c5116b510243c03e36b2ab3021984b45
Jasopaum <jason.paumier@gmail.com>
2020-01-07 16:15:37 +0100

    reduce recursive function complexity (flake8)

--- fhirpipe/transform/fhir.py
+++ fhirpipe/transform/fhir.py
@@ -23,6 +23,39 @@ def create_fhir_object(row, resource_structure):
     return fhir_object
 
 
+def build_fhir_attribute(attribute_structure, row):
+    cols_to_fetch = ([], [])
+    for input_col in attribute_structure["inputColumns"]:
+
+        # If a sql location is provided, then a sql value has been returned
+        if input_col["table"] and input_col["column"]:
+            col_name = build_col_name(
+                input_col["table"], input_col["column"], input_col["owner"]
+            )
+            if input_col["script"]:
+                # If there is a cleaning script, we fetch the value in the cleaned column
+                col_name = new_col_name(input_col["script"], col_name)
+            cols_to_fetch[0].append(col_name)
+
+        # Else retrieve the static value
+        else:
+            cols_to_fetch[1].append(input_col["staticValue"])
+
+    if attribute_structure["mergingScript"]:
+        result = row[
+            new_col_name(attribute_structure["mergingScript"], cols_to_fetch)
+        ]
+    else:
+        result = [row[c] for c in cols_to_fetch[0]]
+        result.extend(cols_to_fetch[1])
+
+    # Unlist
+    if len(result) == 1:
+        result = result[0]
+
+    return result
+
+
 def rec_create_fhir_object(fhir_obj, attribute_structure, row):
     if isinstance(attribute_structure, dict):
         # If there are some inputs, we can build the objects to output
@@ -30,68 +63,32 @@ def rec_create_fhir_object(fhir_obj, attribute_structure, row):
             "inputColumns" in attribute_structure.keys()
             and attribute_structure["inputColumns"]
         ):
-            cols_to_fetch = ([], [])
-            for input_col in attribute_structure["inputColumns"]:
-
-                # If a sql location is provided, then a sql value has been returned
-                if input_col["table"] and input_col["column"]:
-                    col_name = build_col_name(
-                        input_col["table"], input_col["column"], input_col["owner"]
-                    )
-                    if input_col["script"]:
-                        # If there is a cleaning script, we fetch the value in the cleaned column
-                        col_name = new_col_name(input_col["script"], col_name)
-                    cols_to_fetch[0].append(col_name)
-
-                # Else retrieve the static value
-                else:
-                    cols_to_fetch[1].append(input_col["staticValue"])
-
-            if attribute_structure["mergingScript"]:
-                result = row[
-                    new_col_name(attribute_structure["mergingScript"], cols_to_fetch)
-                ]
-            else:
-                result = [row[c] for c in cols_to_fetch[0]]
-                result.extend(cols_to_fetch[1])
-
-            # Unlist
-            if len(result) == 1:
-                result = result[0]
-
+            result = build_fhir_attribute(attribute_structure, row)
             fhir_obj[attribute_structure["name"]] = result
 
         # Otherwise, we can recurse on children
         else:
-            if attribute_structure["type"] == "list":
-                fhir_obj[attribute_structure["name"]] = []
-                for attr in attribute_structure["attributes"]:
-                    child = {}
-                    rec_create_fhir_object(child, attr, row)
+            is_list = attribute_structure["type"].startswith("list")
+
+            fhir_obj[attribute_structure["name"]] = [] if is_list else dict()
+
+            for attr in attribute_structure["attributes"]:
+                child = {} if is_list else fhir_obj[attribute_structure["name"]]
+                rec_create_fhir_object(child, attr, row)
+
+                if is_list:
+                    if attribute_structure["type"] != "list":
+                        child = list(child.values())
+                        if len(child) == 1:
+                            child = child[0]
+
                     fhir_obj[attribute_structure["name"]].append(child)
 
-            elif attribute_structure["type"].startswith("list"):
-                fhir_obj[attribute_structure["name"]] = []
-                for attr in attribute_structure["attributes"]:
-                    child = {}
-                    rec_create_fhir_object(child, attr, row)
-                    values = list(child.values())
-                    if len(values) == 1:
-                        values = values[0]
-                    fhir_obj[attribute_structure["name"]].append(values)
-
-            else:
-                fhir_obj[attribute_structure["name"]] = dict()
-                for attr in attribute_structure["attributes"]:
-                    rec_create_fhir_object(
-                        fhir_obj[attribute_structure["name"]], attr, row
-                    )
-
-                # If the object is a Reference, to we give it to bind_reference
-                if attribute_structure["type"].startswith("Reference"):
-                    bind_reference(
-                        fhir_obj[attribute_structure["name"]], attribute_structure
-                    )
+            # If the object is a Reference, to we give it to bind_reference
+            if attribute_structure["type"].startswith("Reference"):
+                bind_reference(
+                    fhir_obj[attribute_structure["name"]], attribute_structure
+                )
 
     # If the current object is a list, we can repeat the same steps as above for each item
     elif isinstance(fhir_obj, list) and len(fhir_obj) > 0:
