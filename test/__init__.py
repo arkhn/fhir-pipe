f12ba8152f5575052517a2f97bfd9bd1432eb212
Jasopaum <jason.paumier@gmail.com>
2020-01-16 14:47:12 +0100

    cleaning according to comments

--- test/__init__.py
+++
@@ -1,9 +0,0 @@
-import yaml
-import logging
-
-
-with open("config-test.yml", "r") as stream:
-    try:
-        global_config = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        logging.error(exc)
--- test/unit/extract/test_sql.py
+++ test/unit/extract/test_sql.py
@@ -20,3 +20,40 @@ def test_build_sql_query():
         "FROM PATIENTS\n"
         "LEFT JOIN ADMISSIONS ON PATIENTS.SUBJECT_ID=ADMISSIONS.SUBJECT_ID"
     )
+
+    # Without joins
+    cols = [
+        "ADMISSIONS.LANGUAGE",
+        "PATIENTS.DOD",
+        "PATIENTS.SUBJECT_ID",
+    ]  # NOTE: I use a list instead of a set to keep the order of elements
+    joins = {}
+    table = "PATIENTS"
+
+    actual = sql.build_sql_query(cols, joins, table)
+
+    assert actual == (
+        "SELECT ADMISSIONS.LANGUAGE, PATIENTS.DOD, PATIENTS.SUBJECT_ID\n"
+        "FROM PATIENTS\n"
+    )
+
+    # With several joins
+    cols = [
+        "ADMISSIONS.LANGUAGE",
+        "PATIENTS.DOD",
+        "PATIENTS.SUBJECT_ID",
+    ]  # NOTE: I use a list instead of a set to keep the order of elements
+    joins = [
+        ("PATIENTS.SUBJECT_ID", "ADMISSIONS.SUBJECT_ID"),
+        ("PATIENTS.SUBJECT_ID", "MEDICATION.SUBJECT_ID"),
+    ]  # NOTE: I use a list instead of a set to keep the order of elements
+    table = "PATIENTS"
+
+    actual = sql.build_sql_query(cols, joins, table)
+
+    assert actual == (
+        "SELECT ADMISSIONS.LANGUAGE, PATIENTS.DOD, PATIENTS.SUBJECT_ID\n"
+        "FROM PATIENTS\n"
+        "LEFT JOIN ADMISSIONS ON PATIENTS.SUBJECT_ID=ADMISSIONS.SUBJECT_ID\n"
+        "LEFT JOIN MEDICATION ON PATIENTS.SUBJECT_ID=MEDICATION.SUBJECT_ID"
+    )
