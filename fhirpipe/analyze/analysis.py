08b1f43da80d96477215c38b5166a86b81bb079b
Jasopaum <jason.paumier@gmail.com>
2020-02-21 12:07:47 +0100

    forgotten file

---
+++ fhirpipe/analyze/analysis.py
@@ -0,0 +1,9 @@
+class Analysis:
+    def __init__(self):
+        self.primary_key_table = None
+        self.primary_key_column = None
+        self.cols = None
+        self.joins = None
+        self.cleaning = None
+        self.merging = None
+        self.squash_rules = None
