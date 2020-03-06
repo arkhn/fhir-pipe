26a43b8d451a154ef0069842f28975c9da04eb92
Jasopaum <jason.paumier@gmail.com>
2020-03-06 10:37:13 +0100

    Fix bug with arrays of literals (#87)
    * fix bug with arrays of literals
    
    * review comments

--- fhirpipe/transform/fhir.py
+++ fhirpipe/transform/fhir.py
@@ -77,6 +77,12 @@ def build_fhir_object(row, path_attributes_map, index=None):
 
     for path, attr in path_attributes_map.items():
         if attr["inputs"]:
+            # Handle the list of literals case.
+            # If we had a list of literals in the mapping, then handle_array_attributes
+            # will try to create fhir objects with an empty path (remove_root_path removes
+            # what's before the [...] included).
+            if path == "":
+                return fetch_values_from_dataframe(row, attr["inputs"], attr["mergingScript"])
 
             # Find if there is an index in the path
             split_path = path.split(".")
@@ -84,7 +90,7 @@ def build_fhir_object(row, path_attributes_map, index=None):
 
             if position_ind is None:
                 # If we didn't find an index in the path, then we don't worry about arrays
-                val = fetch_values_from_dataframe(row, attr["inputs"], attr["mergingScript"],)
+                val = fetch_values_from_dataframe(row, attr["inputs"], attr["mergingScript"])
                 if isinstance(val, tuple) and len(val) == 1:
                     # If we have a tuple of length 1, we simply extract the value and put it in
                     # the fhir object
@@ -179,7 +185,7 @@ def handle_array_attributes(attributes_in_array, row):
     array = []
     for index in range(length):
         element = build_fhir_object(row, attributes_in_array, index=index)
-        if element:
+        if element is not None:
             array.append(element)
 
     return array
