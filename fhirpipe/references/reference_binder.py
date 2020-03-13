3e8310327b478b2e195676f4c966664d4e28a718
Jasopaum <jason.paumier@gmail.com>
2020-03-13 11:25:08 +0100

    clean: it was not needed to look for the reference type

--- fhirpipe/references/reference_binder.py
+++ fhirpipe/references/reference_binder.py
@@ -22,8 +22,39 @@ class ReferenceBinder:
         self.map_resource_type = {}
 
         # map_resource_references is a dict of the form
-        # {resource_id: [(reference_path, reference_type), ...], ...}
+        # {resource_id: [reference_path, ...], ...}
         self.map_resource_references = defaultdict(list)
+
+    def bind_references(self):
+        for resource_id, reference_paths in self.map_resource_references.items():
+            resource_type = self.map_resource_type[resource_id]
+            instances_with_refs = get_resource_instances(resource_id, resource_type)
+            for instance in instances_with_refs:
+                self.bind_references_for_instance(instance, resource_type, reference_paths)
+
+    def bind_references_for_instance(self, instance, resource_type, reference_paths):
+        for reference_path in reference_paths:
+            sub_fhir_object = self.find_sub_fhir_object(instance, reference_path)
+            identifier = sub_fhir_object["identifier"]
+            reference_type = sub_fhir_object["type"]
+
+            # Get the map for the needed type
+            map = self.get_collection_map(reference_type)
+            value = identifier["value"]
+            # TODO system should have been automatically filled if needed
+            system = identifier["system"] if "system" in identifier else ""
+
+            # If the identifier is in the map, we can fill the reference
+            if (value, system) in map:
+                fhir_id = map[(value, system)]
+                sub_fhir_object["reference"] = f"{reference_type}/{fhir_id}"
+            else:
+                logging.warn(
+                    f"Could perform reference binding to the resource of type {reference_type} "
+                    f"and identifier {(value, system)}."
+                )
+
+        self.fhirstore.update(resource_type, instance["id"], instance)
 
     def get_collection_map(self, fhir_type):
         """ Get a dict {(value, system): fhir_id, ...} for the specified resource type.
@@ -51,63 +82,12 @@ class ReferenceBinder:
 
         return self.indentifiers_store[fhir_type]
 
-    def bind_references(self):
-        for resource_id, references in self.map_resource_references.items():
-            resource_type = self.map_resource_type[resource_id]
-            instances_with_refs = get_resource_instances(resource_id, resource_type)
-            for instance in instances_with_refs:
-                self.bind_references_for_instance(instance, resource_type, references)
-
-    def bind_references_for_instance(self, instance, resource_type, references):
-        for reference_path, reference_type in references:
-            sub_fhir_object = self.find_sub_fhir_object(instance, reference_path)
-            identifier = sub_fhir_object["identifier"]
-
-            # Get the map for the needed type
-            map = self.get_collection_map(reference_type)
-            value = identifier["value"]
-            # TODO system should have been automatically filled if needed
-            system = identifier["system"] if "system" in identifier else ""
-
-            # If the identifier is in the map, we can fill the reference
-            if (value, system) in map:
-                sub_fhir_object["reference"] = map[(value, system)]
-            else:
-                logging.warn(
-                    f"Could perform reference binding to the resource of type {reference_type} "
-                    f"and identifier {(value, system)}."
-                )
-
-        self.fhirstore.update(resource_type, instance["id"], instance)
-
     def add_reference(self, resource_mapping, path):
         """ When a reference is found, this method stores the useful information about it
         to later perform the binding.
         """
-        reference_type = self.find_reference_type(resource_mapping, path)
         self.map_resource_type[resource_mapping["id"]] = resource_mapping["definitionId"]
-        self.map_resource_references[resource_mapping["id"]].append((path, reference_type))
-
-    def find_reference_type(self, resource_mapping, path):
-        """ Get the type of the referenced fhir resource from a mapping and the path
-        of the reference attribute.
-        """
-        path_type = f"{path}.type"
-        try:
-            type_attribute = next(
-                attr for attr in resource_mapping["attributes"] if attr["path"] == path_type
-            )
-        except IndexError:
-            logging.error("Trying to find the reference type at a path which doesn't exist.")
-
-        if (
-            "inputs" not in type_attribute
-            or len(type_attribute["inputs"]) > 1
-            or "staticValue" not in type_attribute["inputs"][0]
-        ):
-            logging.error("Trying to find the reference type for an attribute poorly formatted.")
-
-        return type_attribute["inputs"][0]["staticValue"]
+        self.map_resource_references[resource_mapping["id"]].append(path)
 
     @staticmethod
     def find_sub_fhir_object(instance, path):
