d87463ee26680864542deea235a4087d92537bab
Jasopaum <jason.paumier@gmail.com>
2020-04-21 10:14:06 +0200

    fix

--- fhirpipe/transform/transformer.py
+++ fhirpipe/transform/transformer.py
@@ -15,18 +15,21 @@ class Transformer:
         logging.info("Transforming resource: %s", resource_mapping["definitionId"])
 
         # Change not string value to strings (to be validated by jsonSchema for resource)
-        chunk = chunk.applymap(str)
+        chunk = chunk.applymap(lambda value: str(value) if value is not None else None)
 
         # Apply cleaning scripts and concept map on chunk
         chunk = clean_dataframe(chunk, analysis.attributes, analysis.primary_key_column)
+
+        # We need to remove None values because pandas removes rows with
+        # these on groupby
+        chunk.replace(to_replace=[None], value="None", inplace=True)
 
         # Apply join rule to merge some lines from the same resource
         logging.info("Squashing rows...")
         chunk = squash_rows(chunk, analysis.squash_rules)
 
-        # I need to do this after the squashing because pandas removes rows with
-        # None values on groupby
-        chunk.replace(to_replace=["None"], value=None, inplace=True)
+        # Put None values back
+        chunk.replace(to_replace={"None": None}, inplace=True)
 
         # Apply merging scripts on chunk
         chunk = merge_dataframe(chunk, analysis.attributes, analysis.primary_key_column)
